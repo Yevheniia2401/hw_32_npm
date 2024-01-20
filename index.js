563f4a2616ec812a43d20de2df2e31d2ed4a02a5
Yevheniia2401 <yevhenia2401@gmail.com>
2024-01-20 22:04:04 +0200

    completed homework

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict'
+const findSum = function (a,b){
+    const result = a+b;
+    return result
+}
+
+const findDiv = function (a,b) {
+    const result = a-b;
+    return result
+}
+const findMult = function (a,b) {
+    const result = a+b;
+    return result
+}
